2e1be7122e9e19714692be3735208f3f0369a6cf
viernes2002 <jomihergom@gmail.com>
2024-02-12 13:31:11 -0500

    Codigos comit

--- area.py
+++ area.py
@@ -2,6 +2,6 @@ import math
 def area(radio):
     a = math.pi * radio**2
     return a
-radio= float(imput("ingrese el radio para calcular el area : "))
+radio= float(input("ingrese el radio para calcular el area : "))
 a = area(radio)
 print("el area del circulo es :",a)
---
+++ par.py
@@ -0,0 +1,8 @@
+def parimp(numero):
+    if numero % 2 = 0 :
+        return "par"
+    else :
+        return "Impar"
+numero = int(input("Ingrese el numero para determinar si es Par o Impar"))
+B = parimp(numero)
+print("El numero  ",numero "Es ",B)
