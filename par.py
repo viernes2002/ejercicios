6824b6b57c45abf89c07f2871c42b6b4f9df7699
viernes2002 <jomihergom@gmail.com>
2024-02-12 13:38:51 -0500

    Codigos comit

--- par.py
+++ par.py
@@ -1,8 +1,8 @@
 def parimp(numero):
-    if numero % 2 = 0 :
+    if numero % 2 == 0 :
         return "par"
     else :
         return "Impar"
-numero = int(input("Ingrese el numero para determinar si es Par o Impar"))
+numero = int(input("Ingrese el numero para determinar si es Par o Impar :"))
 B = parimp(numero)
-print("El numero  ",numero "Es ",B)
+print("El numero ",numero, "Es ",B)
