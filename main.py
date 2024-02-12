76e96929fbb05de1e2679de7bd216a908e46c4d8
viernes2002 <jomihergom@gmail.com>
2024-02-12 13:09:31 -0500

    start comit

---
+++ main.py
@@ -0,0 +1,7 @@
+import math
+def area(radio):
+    a = math.pi * radio**2
+    return a
+radio= float(imput("ingrese el radio para calcular el area : "))
+a = area(radio)
+print("el area del circulo es :",a)
